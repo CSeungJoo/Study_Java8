1e9f9c71106817f30ed3613f2d7b43570080aa4e
CSeungJoo <seungjoo13579@gmail.com>
2024-03-08 06:35:01 +0900

    Stream 소개

--- src/main/java/kr/pe/sjc/study_java8/App.java
+++ src/main/java/kr/pe/sjc/study_java8/App.java
@@ -5,17 +5,24 @@ import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.function.UnaryOperator;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class App {
 
     public static void main(String[] args) {
-        List<String> name = new ArrayList<>();
-        name.add("Keesun");
-        name.add("Whiteship");
-        name.add("Toby");
-        name.add("Foo");
-        name.add("CSeungJoo");
+        List<String> names = new ArrayList<>();
+        names.add("Keesun");
+        names.add("Whiteship");
+        names.add("Toby");
+        names.add("Foo");
+        names.add("CSeungJoo");
 
-        name.forEach(System.out::println);
+        List<String> collect = names.stream()
+                .map(s -> {
+                    System.out.println(s +" "+ Thread.currentThread().getName());
+                    return s.toUpperCase();
+                }).collect(Collectors.toList());
+        names.forEach(System.out::println);
     }
 }
